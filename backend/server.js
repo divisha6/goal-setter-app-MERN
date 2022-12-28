cbbf210d16e73f7885623bcf88e3b6a428f68c84
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2022-12-28 22:57:21 +0530

    set .env file

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,15 @@
 // entry point to our server
 
-console.log("Hello World");
+const express = require("express"); //bringing express.js that is our backend web framework
+const dotenv = require("dotenv").config(); //so that we can have env variables
+//config: allow us to have a dotenv file without variables in it
+
+const port= process.env.PORT || 5000;
+//port: which port we want server to run on
+
+
+const app= express(); //initializing express
+
+app.listen(port, ()=> console.log(`Server started on port ${port}`))
+
+// start from first route
